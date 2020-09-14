2dfb98d0a77952c90734f60818b504b50868a487
JeffMony <jeffmony@163.com>
2020-09-14 13:40:05 +0800

    引入最新的mp4parser库

--- app/src/main/java/com/jeffmony/mp4demo/MainActivity.java
+++ app/src/main/java/com/jeffmony/mp4demo/MainActivity.java
@@ -3,12 +3,30 @@ package com.jeffmony.mp4demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mPrintMp4View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mPrintMp4View = findViewById(R.id.print_mp4_btn);
+
+        mPrintMp4View.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mPrintMp4View) {
+
+        }
     }
 }
